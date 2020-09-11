4f409b46ba8fcef6412343cfa01624b61a9d8f4a
joshuasearle <joshryansearle@gmail.com>
2020-09-11 14:12:24 +1000

    Add movie routes

--- routers/router.js
+++ routers/router.js
@@ -5,12 +5,21 @@ const movies = require('../controllers/movie');
 
 const router = express.Router();
 
+// Actor routes
 router.get('/actors', actors.getAll);
 router.post('/actors', actors.createOne);
 router.get('/actors/:id', actors.getOne);
 router.put('/actors/:id', actors.updateOne);
 router.delete('/actors/:id', actors.deleteOne);
 
+// Movies routes
+router.get('/movies', movies.getAll);
+router.get('/movies', movies.getAll);
+router.post('/movies', movies.createOne);
+router.get('/movies/:id', movies.getOne);
+router.put('/movies/:id', movies.updateOne);
+
+// 404 route
 router.use('/', (req, res) => res.status(404).json());
 
 module.exports = router;
