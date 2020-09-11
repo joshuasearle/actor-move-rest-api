24408a38deff886971ae2f5cf31987ddb910b62e
joshuasearle <joshryansearle@gmail.com>
2020-09-11 14:03:59 +1000

    Add controller functions to router

--- routers/router.js
+++ routers/router.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
-const actorController = require('../controllers/actor');
-const movieController = require('../controllers/movie');
+const actors = require('../controllers/actor');
+const movies = require('../controllers/movie');
 
 const router = express.Router();
+
+router.get('/actors', actors.getAll);
+router.post('/actors', actors.createOne);
+router.get('/actors/:id', actors.getOne);
+router.put('/actors/:id', actors.updateOne);
+router.delete('/actors/:id', actors.deleteOne);
+
+router.use('/', (req, res) => res.status(404).json());
 
 module.exports = router;
