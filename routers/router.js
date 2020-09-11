26f737b5735eeb9492cb1e13eab40c3a1c405687
joshuasearle <joshryansearle@gmail.com>
2020-09-11 14:00:40 +1000

    Add router boilerplate

--- routers/router.js
+++ routers/router.js
@@ -1,2 +1,8 @@
+const express = require('express');
+
 const actorController = require('../controllers/actor');
 const movieController = require('../controllers/movie');
+
+const router = express.Router();
+
+module.exports = router;
